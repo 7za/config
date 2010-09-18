76fc953ccb81cc8245e94ff7c53dc29e35b300a3
Frederic Ferrandis <frederic.ferrandis@openwide.fr>
2010-09-18 14:33:45 +0200

    initial configuration commit

---
+++ xmonad/.xmonad/xmonadinit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+trayer --edge top --align right --SetDockType true --SetPartialStrut true \
+     --expand true --width 10 --transparent true --tint 0x191970 --height 12 &
+ 
+nm-applet --sm-disable &
+gvolwheel &
+mail-notification &
+gnome-screensaver &
+gnome-power-manager
+
+
+
